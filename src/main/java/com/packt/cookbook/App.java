63b8dca0abf5f0c5dbccf41d711555f4ec797a93
Hui Zhou <davidhuizhou@gmail.com>
2015-11-18 22:55:26 -0500

    no message

---
+++ src/main/java/com/packt/cookbook/App.java
@@ -0,0 +1,14 @@
+package com.packt.cookbook;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void testCheckStyle(String style) {
+        System.out.println(style);
+    }
+}
